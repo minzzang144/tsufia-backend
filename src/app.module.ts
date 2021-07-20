b8812360eb0126a96826f7d59ff4ecfabea9f5db
MinChan Lee <shigatsu970704@gmail.com>
2021-07-20 15:50:29 +0900

    [Set] TypeOrm Configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'dummy-password',
+      database: 'tsufia-database',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
