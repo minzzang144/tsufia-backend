b28a32ebf5d013b92713f1f7e60c3b9870c2774c
MinChan Lee <shigatsu970704@gmail.com>
2021-07-21 15:17:23 +0900

    [Feat] Create User

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,5 +9,22 @@ import { CreateUserInputDto, CreateUserOutputDto } from '@users/dtos/create-user
 export class UsersService {
   constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) {}
 
-  async createUser(createUserInputDto: CreateUserInputDto): Promise<CreateUserOutputDto> {}
+  async createUser(createUserInputDto: CreateUserInputDto): Promise<CreateUserOutputDto> {
+    try {
+      const { email, firstName, lastName, password, checkPassword } = createUserInputDto;
+      const userExist = await this.userRepository.findOne({ email });
+
+      // 유저 중복 & 패스워드 일치 검사
+      if (userExist) return { ok: false, error: '이미 존재하는 사용자 이름입니다.' };
+      if (password !== checkPassword) return { ok: false, error: '패스워드가 일치하지 않습니다.' };
+
+      // 유저 생성
+      const user = this.userRepository.create({ email, firstName, lastName, password });
+      await this.userRepository.save(user);
+
+      return { ok: true };
+    } catch (error) {
+      return { ok: false, error: '계정을 생성할 수 없습니다.' };
+    }
+  }
 }
