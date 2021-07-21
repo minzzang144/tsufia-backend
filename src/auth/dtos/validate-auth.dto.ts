91f98491755f5cddac31a5ecf9768247a4a229f5
MinChan Lee <shigatsu970704@gmail.com>
2021-07-21 17:50:54 +0900

    [Add] Validate Auth Dto

---
+++ src/auth/dtos/validate-auth.dto.ts
@@ -0,0 +1,11 @@
+import { CoreOutput } from '@common/dtos/core.dto';
+import { PickType } from '@nestjs/mapped-types';
+import { User } from '@users/entities/user.entity';
+import { IsOptional } from 'class-validator';
+
+export class ValidateAuthInputDto extends PickType(User, ['email', 'password']) {}
+
+export class ValidateAuthOutputDto extends CoreOutput {
+  @IsOptional()
+  user?: User;
+}
