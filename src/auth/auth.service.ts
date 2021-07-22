95dd24a416a68e0f79213b7135739370aec18807
MinChan Lee <shigatsu970704@gmail.com>
2021-07-22 13:27:03 +0900

    [Feat] Validate User

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+import { ValidateAuthInputDto, ValidateAuthOutputDto } from '@auth/dtos/validate-auth.dto';
 import { UsersService } from '@users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly usersService: UsersService) {}
+
+  /* Validate User */
+  async validateUser(validateAuthInputDto: ValidateAuthInputDto): Promise<ValidateAuthOutputDto> {
+    try {
+      const { email, password } = validateAuthInputDto;
+      const { user } = await this.usersService.findOneUser({ email });
+      if (!user) return { ok: false, error: '이메일 계정이 존재하지 않습니다.' };
+      if (user && user.password !== password) {
+        return { ok: false, error: '패스워드가 일치하지 않습니다.' };
+      }
+      return { ok: true, user };
+    } catch (error) {
+      return { ok: false, error: '로그인 인증에 실패하였습니다.' };
+    }
+  }
 }
