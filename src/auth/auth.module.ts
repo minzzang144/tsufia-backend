1a99ac8bf882ddc9a0ba7eb3f9a41d0ed9af863b
MinChan Lee <shigatsu970704@gmail.com>
2021-07-22 15:47:18 +0900

    [Feat] Login from Passport Local

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 
+import { AuthController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
+import { LocalStrategy } from '@auth/strategies/local.strategy';
 import { UsersModule } from '@users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthService],
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
