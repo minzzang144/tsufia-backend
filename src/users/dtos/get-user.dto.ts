e56314807b2a16fb449154a115e2a34c7e67a988
MinChan Lee <shigatsu970704@gmail.com>
2021-07-23 16:55:37 +0900

    [Fix] Get User Dto(Add id)

--- src/users/dtos/get-user.dto.ts
+++ src/users/dtos/get-user.dto.ts
@@ -1,10 +1,10 @@
 import { CoreOutput } from '@common/dtos/core.dto';
-import { PickType } from '@nestjs/mapped-types';
+import { PartialType, PickType } from '@nestjs/mapped-types';
 
 import { User } from '@users/entities/user.entity';
 import { IsOptional } from 'class-validator';
 
-export class GetUserInputDto extends PickType(User, ['email']) {}
+export class GetUserInputDto extends PartialType(PickType(User, ['id', 'email'])) {}
 
 export class GetUserOutputDto extends CoreOutput {
   @IsOptional()
