6ee666f2ec27ea752f8086a8f64beb681234e20a
MinChan Lee <shigatsu970704@gmail.com>
2021-07-24 18:20:17 +0900

    [Add] Get Will Patch User Dto

---
+++ src/users/dtos/get-will-patch-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsOptional } from 'class-validator';
+
+import { CoreOutput } from '@common/dtos/core.dto';
+import { User } from '@users/entities/user.entity';
+
+export type RequestWithUserData = Request & { user: User };
+
+export class GetWillPatchUserOutput extends CoreOutput {
+  @IsOptional()
+  user?: User;
+}
