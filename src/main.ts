ee160492ba118eb66a0120735ffd0cc3ae26ce05
MinChan Lee <shigatsu970704@gmail.com>
2021-07-29 15:42:15 +0900

    [Set] Cookie Parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   await app.listen(3000);
 }
 bootstrap();
