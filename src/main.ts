0faa1cde1c6c92d933267321ede952227cdbc8dd
MinChan Lee <shigatsu970704@gmail.com>
2021-07-30 12:35:10 +0900

    [Fix] NestJS Connection Port Number

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
