48dea83304b1ea6ce4cca42c82e658f899014163
MinChan Lee <shigatsu970704@gmail.com>
2021-07-30 17:33:56 +0900

    [Add] Google User Interface

--- src/auth/auth.interface.ts
+++ src/auth/auth.interface.ts
@@ -2,14 +2,27 @@ import { Request } from 'express';
 
 import { ValidateAuthOutputDto } from '@auth/dtos/validate-auth.dto';
 
+/* Local Login Strategy */
 export type RequestWithUser = Request & { user: ValidateAuthOutputDto };
 
+/* Silent Refresh */
 export interface RefreshTokenPayload {
   id: number;
   iat: string;
   exp: string;
 }
 
+/* JWT Strategy */
 export interface Payload {
   id: number;
 }
+
+/* Google Strategy */
+type GoogleUser = {
+  email: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+export type GoogleRequest = Request & { user: GoogleUser };
