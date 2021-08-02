ed6d60d183e24b15a76b3b811cc744ab524095f5
MinChan Lee <shigatsu970704@gmail.com>
2021-08-02 10:39:57 +0900

    [Set] TypeOrmMoudle with User, GoogleStrategy Provider, Google Controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,15 +2,19 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
-import { AuthController } from '@auth/auth.controller';
+import { AuthController, GoogleController } from '@auth/auth.controller';
 import { AuthService } from '@auth/auth.service';
 import { LocalStrategy } from '@auth/strategies/local.strategy';
 import { UsersModule } from '@users/users.module';
 import { JwtStrategy } from '@auth/strategies/jwt.strategy';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GoogleStrategy } from '@auth/strategies/google.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@users/entities/user.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([User]),
     UsersModule,
     PassportModule,
     JwtModule.registerAsync({
@@ -24,7 +28,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       }),
     }),
   ],
-  providers: [AuthService, LocalStrategy, JwtStrategy],
-  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy, JwtStrategy, GoogleStrategy],
+  controllers: [AuthController, GoogleController],
 })
 export class AuthModule {}
