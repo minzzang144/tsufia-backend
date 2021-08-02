15ac4956cc769b35521057a1d458c4ab89d524ea
MinChan Lee <shigatsu970704@gmail.com>
2021-08-02 10:39:51 +0900

    [Add] Google Controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
-import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { Request, Response } from 'express';
 
 import { AuthService } from '@auth/auth.service';
-import { LocalAuthGuard } from '@auth/guards/local-auth.guard';
+import { GoogleRequest, RequestWithUser } from '@auth/auth.interface';
+import { GoogleLoginAuthOutputDto } from '@auth/dtos/google-login-auth.dto';
 import { LoginAuthOutputDto } from '@auth/dtos/login-auth.dto';
-import { RequestWithUser } from '@auth/auth.interface';
+import { LocalAuthGuard } from '@auth/guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +25,25 @@ export class AuthController {
   @Post('silent-refresh')
   async silentRefresh(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
     return this.authService.silentRefresh(req, res);
+  }
+}
+
+@Controller('google')
+export class GoogleController {
+  constructor(private readonly authService: AuthService) {}
+
+  /* Get Google Auth */
+  @Get()
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() _req: Request) {}
+
+  /* Get Google Auth Callback */
+  @Get('callback')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthCallback(
+    @Req() req: GoogleRequest,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<GoogleLoginAuthOutputDto> {
+    return this.authService.googleLogin(req, res);
   }
 }
