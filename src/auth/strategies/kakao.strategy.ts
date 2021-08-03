309f2d4231bcde28125fa95147570a77dd2e592f
MinChan Lee <shigatsu970704@gmail.com>
2021-08-03 18:26:06 +0900

    [Fix] Kakao Strategy Return Type

--- src/auth/strategies/kakao.strategy.ts
+++ src/auth/strategies/kakao.strategy.ts
@@ -20,9 +20,11 @@ export class KakaoStrategy extends PassportStrategy(Strategy) {
     done: (error: any, user?: any, info?: any) => void,
   ) {
     try {
-      console.log(profile);
+      const { _json } = profile;
       const user = {
-        id: profile.id,
+        email: _json.kakao_account.email,
+        nickname: _json.properties.nickname,
+        photo: _json.properties.thumbnail_image,
       };
       done(null, user);
     } catch (error) {
