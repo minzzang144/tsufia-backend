fdc5be62d6029bf90b6b2a4d9ee34d6bfeeee196
MinChan Lee <shigatsu970704@gmail.com>
2021-08-04 12:32:08 +0900

    [Fix] Create User Service(Provider)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { User } from '@users/entities/user.entity';
+import { Provider, User } from '@users/entities/user.entity';
 import { CreateUserInputDto, CreateUserOutputDto } from '@users/dtos/create-user.dto';
 import { GetUserInputDto, GetUserOutputDto } from '@users/dtos/get-user.dto';
 import { GetWillPatchUserOutput } from '@users/dtos/get-will-patch-user.dto';
@@ -21,11 +21,11 @@ export class UsersService {
       const userExist = await this.userRepository.findOne({ email });
 
       // 유저 중복 & 패스워드 일치 검사
-      if (userExist) return { ok: false, error: '이미 존재하는 사용자 이름입니다.' };
+      if (userExist) return { ok: false, error: '이미 존재하는 사용자 계정입니다.' };
       if (password !== checkPassword) return { ok: false, error: '패스워드가 일치하지 않습니다.' };
 
       // 유저 생성
-      const user = this.userRepository.create({ email, firstName, lastName, password });
+      const user = this.userRepository.create({ email, firstName, lastName, password, provider: Provider.Local });
       await this.userRepository.save(user);
 
       return { ok: true };
