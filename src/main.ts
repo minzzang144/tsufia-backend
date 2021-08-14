79060270b3ce2ea6a11ec19cc0d9f0425b928a5d
MinChan Lee <shigatsu970704@gmail.com>
2021-08-14 12:30:56 +0900

    [Set] Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
+  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
   await app.listen(4000);
 }
 bootstrap();
