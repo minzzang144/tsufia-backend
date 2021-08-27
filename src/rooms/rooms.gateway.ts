15d42a6edcf173395a8b7fc20d14a8c6b1ff99be
MinChan Lee <shigatsu970704@gmail.com>
2021-08-27 12:41:37 +0900

    [Make] Different Events for User in the Room and User outside the Room

--- src/rooms/rooms.gateway.ts
+++ src/rooms/rooms.gateway.ts
@@ -8,48 +8,53 @@ import { Room } from '@rooms/entities/room.entity';
 export class RoomsGateway {
   @WebSocketServer() server: Server;
 
-  // Get Rooms
-  @SubscribeMessage('rooms:get:server')
-  handleGetRooms(@MessageBody() data: Room[], @ConnectedSocket() client: Socket) {
-    this.server.emit('rooms:get:client', data);
+  // Join Rooms
+  @SubscribeMessage('rooms:join:server')
+  handleJoinRoom(@ConnectedSocket() client: Socket) {
+    client.join('rooms');
+    console.log(this.server.sockets.adapter.rooms);
   }
 
   // Create Room
   @SubscribeMessage('rooms:create:server')
   handleCreateRoom(@MessageBody() data: Room, @ConnectedSocket() client: Socket) {
+    client.leave('rooms');
     client.join(`rooms/${data.id}`);
-    this.server.emit('rooms:create:client', data);
+    console.log(this.server.sockets.adapter.rooms);
+    this.server.to('rooms').emit('rooms:create:client', data);
   }
 
   // Update Rooms
   @SubscribeMessage('rooms:update:server')
   handleUpdateRoom(@MessageBody() data: Room, @ConnectedSocket() client: Socket) {
-    this.server.emit('rooms:update:client', data);
+    this.server.to('rooms').emit('rooms:update:client', data);
+    this.server.to(`rooms/${data.id}`).emit('rooms:update:each-client', data);
   }
 
   // Remove Room
   @SubscribeMessage('rooms:remove:server')
   handleRemoveRoom(@MessageBody() data: number, @ConnectedSocket() client: Socket) {
-    console.log(client.rooms);
-    this.server.emit('rooms:remove:client', data);
-    this.server.to(`rooms/${data}`).emit('rooms:remove:each-client');
+    this.server.to('rooms').emit('rooms:remove:client', data);
     client.leave(`rooms/${data}`);
+    console.log(this.server.sockets.adapter.rooms);
   }
 
   // Enter Room
   @SubscribeMessage('rooms:enter:server')
   handleEnterRoom(@MessageBody() data: Room, @ConnectedSocket() client: Socket) {
+    client.leave('rooms');
     client.join(`rooms/${data.id}`);
-    this.server.emit('rooms:enter:client', data);
+    console.log(this.server.sockets.adapter.rooms);
+    this.server.to('rooms').emit('rooms:enter:client', data);
     this.server.to(`rooms/${data.id}`).emit('rooms:enter:each-client', data);
   }
 
   // Leave Room
   @SubscribeMessage('rooms:leave:server')
   handleLeaveRoom(@MessageBody() data: Room, @ConnectedSocket() client: Socket) {
-    console.log(client.rooms);
     client.leave(`rooms/${data.id}`);
-    this.server.emit('rooms:leave:client', data);
+    console.log(this.server.sockets.adapter.rooms);
+    this.server.to('rooms').emit('rooms:leave:client', data);
     this.server.to(`rooms/${data.id}`).emit('rooms:leave:each-client', data);
   }
 }
