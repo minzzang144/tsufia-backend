20e435bbabd1eafa3fb69eb16b9ffa298ebba6ba
MinChan Lee <shigatsu970704@gmail.com>
2021-09-19 14:53:31 +0900

    [Feat] Leave and Remove Room when Disconnected

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -9,8 +9,12 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
+import { RoomsService } from '@rooms/rooms.service';
+
 @WebSocketGateway(undefined, { cors: { origin: 'http:://localhost:3000', credentials: true } })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  constructor(private readonly roomService: RoomsService) {}
+
   @WebSocketServer() server: Server;
   private logger: Logger = new Logger('AppGateway');
 
@@ -18,7 +22,21 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
     this.logger.log('Init');
   }
 
-  handleDisconnect(client: Socket) {
+  async handleDisconnect(client: Socket) {
+    if (client['roomId']) {
+      const deleteResponse = await this.roomService.deleteRoom({ id: +client.data.user.id });
+      const { ok: deleteOk, roomId } = deleteResponse;
+      if (deleteOk) {
+        this.server.to('rooms').emit('rooms:remove:client', roomId);
+      }
+      const leaveResponse = await this.roomService.leaveRoom({ id: +client.data.user.id }, client['roomId']);
+      const { ok: leaveOk, room } = leaveResponse;
+      if (leaveOk) {
+        this.server.to('rooms').emit('rooms:leave:client', room);
+        client.broadcast.to(`rooms/${room.id}`).emit('rooms:leave:broadcast-client', client.data.user);
+        this.server.to(`rooms/${room.id}`).emit('rooms:leave:each-client', room);
+      }
+    }
     this.logger.log(`Client disconnected: ${client.id}`);
   }
 
